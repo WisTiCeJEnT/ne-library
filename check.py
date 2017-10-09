d2e989db540ed0c9064b3163614ae3dca4969bd7
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-10-09 10:32:28 +0700

    :art: improving leapyear

--- check.py
+++ check.py
@@ -1,12 +1,6 @@
 def LeapYear(y):
-    ans = True
-    if(y%4==0):
-        if(y%100==0):
-            ans = False
-            if(y%400==0):
-                ans = True
-        else:
+    ans = False
+    if y%4==0 :
+        if y%100!=0 or y%400==0:
             ans = True
-    else:
-        ans = False
     return ans
